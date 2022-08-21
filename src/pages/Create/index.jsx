1129fe499bb0ed62f167b34e482822de387b2fb4
Angela Caldas <aes.caldas@gmail.com>
2022-08-21 19:08:01 -0300

    styling

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -5,11 +5,11 @@ import './style.sass';
 const Create = () => {
     return (
         <section className="create">
-            <div>
+            <div className='create__sections'>
                 <h2>Título</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt ut maxime magni unde fuga id modi expedita, earum reiciendis ducimus optio natus amet sequi quod. Cumque iste voluptatum numquam provident.</p>
             </div>
-            <div>
+            <div className='create__sections'>
                 <h2>Tarefas</h2>
                 {/* Criar componente andamento percentual */}
                 {/* Para os checkbox, criar componente */}
@@ -23,11 +23,11 @@ const Create = () => {
                 </div>
                 <Button>Criar tarefa</Button>
             </div>
-            <div>
+            <div className='create__sections'>
                 <h2>Notas</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi deserunt minus necessitatibus repellat totam provident quisquam ab omnis cupiditate repudiandae asperiores, optio sequi excepturi doloremque maxime. Obcaecati excepturi tempore a. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem distinctio optio eaque debitis, assumenda illo autem eos voluptatibus, veniam, reprehenderit est minima cum sit corrupti sed? Tenetur rerum incidunt molestiae.</p>
             </div>
-            <div>
+            <div className='create__buttons'>
                 <Button>Salvar tarefa</Button>
                 <Button warn={true}>Excluir tarefa</Button>
             </div>
