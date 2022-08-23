952d420ea8b06d2c55e861b893ebe215ec6b343a
Angela Caldas <aes.caldas@gmail.com>
2022-08-23 16:51:43 -0300

    Card page component

--- src/pages/Card/index.jsx
+++ src/pages/Card/index.jsx
@@ -2,9 +2,10 @@
 import '../Create/style.sass';
 
 // dependencies
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
+import { FaWindowClose } from 'react-icons/fa';
 
 // components
 import Button from '../../components/Button';
@@ -12,70 +13,45 @@ import Button from '../../components/Button';
 const Card = () => {
     const { id } = useParams();
     const url = `http://localhost:8000/cards/${id}`;
-    const { data: card, loading } = useFetch(url);
-
-    const [title, setTitle] = useState({ name: '', exists: false });
-    const [description, setDescription] = useState({ name: '', exists: false });
-    const [notes, setNotes] = useState({ name: '', exists: false });
-
-
-    function handleSubmit(e, infoType) {
-        e.preventDefault();
-        if (infoType === 'title') setTitle({ name: title, exists: true });
-        if (infoType === 'description') setDescription({ name: description, exists: true });
-        if (infoType === 'notes') setNotes({ name: notes, exists: true });
-    };
+    const { data: card, loading, error } = useFetch(url);
 
     return (
         <section className="create">
-            <div className='create__sections'>
-                <form onSubmit={(e) => handleSubmit(e, 'title')}>
-                    {!title.exists && (
-                        <input type="text" className='create__sections--input' onChange={(e) => setTitle(e.target.value)} placeholder='Digite um título' />
-                    )}
-                    {title.exists && <h2 onClick={() => setTitle({ exists: false })}>{title.name}</h2>}
-                </form>
-                <form onSubmit={(e) => handleSubmit(e, 'description')}>
-                    {!description.exists && (
-                        <>
-                            <textarea className='create__sections--textarea' onChange={(e) => setDescription(e.target.value)} placeholder='Digite uma descrição'></textarea>
-                            {description.length > 0 && <Button type='submit'>Salvar</Button>}
-                        </>
-                    )}
-                    {description.exists && <p onClick={() => setDescription({ exists: false })}>{description.name}</p>}
-                </form>
-            </div>
-            <div className='create__sections'>
-                <h2>Tarefas</h2>
-                {/* Criar componente andamento percentual */}
-                {/* Para os checkbox, criar componente */}
-                <div>
-                    <input type="checkbox" name="tarefa1" value='tarefa1' id="tarefa1" />
-                    <label htmlFor="tarefa1">Tarefa 1</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="tarefa1" value='tarefa1' id="tarefa1" />
-                    <label htmlFor="tarefa1">Tarefa 2</label>
-                </div>
-                <br />
-                <input type="text" className='create__sections--input' placeholder='Criar uma tarefa' />
-            </div>
-            <div className='create__sections'>
-                <h2>Notas</h2>
-                <form onSubmit={(e) => handleSubmit(e, 'notes')}>
-                    {!notes.exists && (
-                        <>
-                            <textarea className='create__sections--textarea' onChange={(e) => setNotes(e.target.value)} placeholder='Digite suas observações'></textarea>
-                            {notes.length > 0 && <Button type='submit'>Salvar</Button>}
-                        </>
-                    )}
-                    {notes.exists && <p onClick={() => setNotes({ exists: false })}>{notes.name}</p>}
-                </form>
-            </div>
-            <div className='create__buttons'>
-                <Button>Salvar tarefa</Button>
-                <Button warn={true}>Excluir tarefa</Button>
-            </div>
+            {loading && <p>Carregando...</p>}
+            {error && <p>{error}</p>}
+            {card && (
+                <>
+                    <Link to='/'>
+                        <FaWindowClose className='create__close' size='1.75rem' />
+                    </Link>
+                    <div className='create__sections'>
+                        <h2 /*onClick={() => setTitle({ exists: false })}*/>{card.title}</h2>
+                        <p /*onClick={() => setDescription({ exists: false })}*/>{card.description}</p>
+                    </div>
+                    <div className='create__sections'>
+                        <h2>Tarefas</h2>
+                        {card.tasks.map((item) => (
+                            <div key={item.id}>
+                                <input type="checkbox" name={`task${item.id}`} value={`task${item.id}`} id={`task${item.id}`} />
+                                {
+                                    item.task.startsWith('http') ?
+                                        <a className='create__sections--link' href={item.task}>{item.task}</a>
+                                        :
+                                        <label>{item.task}</label>
+                                }
+                            </div>
+                        ))}
+                    </div>
+                    <div className='create__sections'>
+                        <h2>Notas</h2>
+                        <p /*onClick={() => setNotes({ exists: false })}*/>{card.notes}</p>
+                    </div>
+                    <div className='create__buttons'>
+                        <Button>Salvar tarefa</Button>
+                        <Button warn={true}>Excluir tarefa</Button>
+                    </div>
+                </>
+            )}
         </section>
     );
 };
